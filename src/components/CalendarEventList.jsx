f41b8c75693f10f6ab1d3e748d98538fe19f1f4b
Alejandro Chacón <maledict@protonmail.com>
2024-05-03 16:33:00 -0600

    Responsive design.

--- src/components/CalendarEventList.jsx
+++ src/components/CalendarEventList.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useMemo } from "react";
+
 import {
   Stack,
   Typography,
@@ -6,8 +8,10 @@ import {
   ListItem,
   ListItemButton,
   ListItemText,
+  Drawer,
 } from "@mui/material";
 
+import useWindowSize from "../hooks/useWindowSize";
 function parseDate(date) {
   // Convert the date object to a string, showing only the month
   return `${date.toLocaleString("es-MX", {
@@ -24,35 +28,84 @@ function formatTime(date) {
   return `${hours}:${minutes}`;
 }
 
-export default function CalendarEventList({ selectedEvents, selectedDate }) {
+export default function CalendarEventList({
+  selectedEvents,
+  selectedDate,
+  isOpen,
+}) {
+  const { width } = useWindowSize();
+  const isMobile = width < 900; // Define mobile view as less than 768px
+
+  const EventList = () => {
+    return (
+      <Stack bgcolor={"white"} height={"100%"} borderRadius={1}>
+        <Typography variant="h6" padding={2}>
+          {parseDate(selectedDate)}
+        </Typography>
+        <Divider></Divider>
+        <List>
+          {selectedEvents.map((FEIEvent) => (
+            <ListItem
+              className="calendar-event"
+              key={FEIEvent.id}
+              disablePadding
+            >
+              <ListItemButton>
+                <Stack direction={"row"} spacing={4}>
+                  {" "}
+                  <ListItemText
+                    primary={formatTime(FEIEvent.start)}
+                    secondary=" "
+                  />
+                  <ListItemText
+                    sx={{
+                      width: "100%",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                    primary={FEIEvent.title}
+                    secondary={FEIEvent.space}
+                  />
+                </Stack>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Stack>
+    );
+  };
+
   return (
-    <div className=" white">
-      <Typography variant="h6">{parseDate(selectedDate)}</Typography>
-      <Divider></Divider>
-      <List>
-        {selectedEvents.map((FEIEvent) => (
-          <ListItem className="calendar-event" key={FEIEvent.id} disablePadding>
-            <ListItemButton>
-              <Stack direction={"row"} spacing={4}>
-                {" "}
-                <ListItemText
-                  primary={formatTime(FEIEvent.start)}
-                  secondary=" "
-                />
-                <ListItemText
-                  sx={{
-                    width: "100%",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                  }}
-                  primary={FEIEvent.title}
-                  secondary={FEIEvent.space}
-                />
-              </Stack>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
+    <>
+      {isMobile ? (
+        <Drawer
+          anchor="right"
+          variant="temporary"
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          open={isOpen}
+          sx={{
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: 300,
+            },
+          }}
+        >
+          <EventList></EventList>
+        </Drawer>
+      ) : (
+        <Stack
+          width={"50%"}
+          flexGrow={1}
+          paddingLeft={3}
+          sx={{
+            display: { md: "block", xs: "none" },
+          }}
+        >
+          <EventList></EventList>
+        </Stack>
+      )}
+    </>
   );
 }
---
+++ src/hooks/useWindowSize.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export default function useWindowSize(debounceTime = 100) {
+    const [size, setSize] = useState({ width: window.innerWidth });
+
+    useEffect(() => {
+        let timeoutId = null;
+
+        const handleResize = () => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => setSize({ width: window.innerWidth }), debounceTime);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            clearTimeout(timeoutId);
+        };
+    }, [debounceTime]);
+
+    return size;
+}
